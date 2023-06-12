c9cb0a46825a49d6e9309214ed250d3027b94c34
Kevin Ly <lykt2@vcu.edu>
2023-06-11 20:49:02 -0400

    Project structure organization and added a navbar component to be developed

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <div>
+            <p>Hello, Kevin Ly</p>
+            <p>Hello, Kevin Ly</p>
+            <p>Hello, Kevin Ly</p>
+            <p>Hello, Kevin Ly</p>
+            <p>Hello, Kevin Ly</p>
+        </div>
+    );
+}
+
+export default Navbar
